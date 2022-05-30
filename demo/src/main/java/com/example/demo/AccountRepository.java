bd89e10f41941e01ba8ad951d30a1cbbb4e44140
Dias Bagzat <diasbagzat1@gmail.com>
2022-05-30 13:36:19 +0600

    BankApp V3.0

---
+++ demo/src/main/java/com/example/demo/AccountRepository.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import com.example.demo.account.Account;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AccountRepository extends CrudRepository<Account, String> {
+
+    @Query("select id from Account a where  a.account_type =:accountType and a.client_id = :clientId")
+    List<Account> getAccountsByAccountType(String clientId, String accountType);
+
+    @Modifying
+    @Query("insert into Account values(:id, :accountType, :clientID, 0.0,  :withdrawAllowed)")
+    void insert(String accountType, String id, String clientID, Boolean withdrawAllowed);
+
+    @Modifying
+    @Query("UPDATE Account a SET a.balance = a.balance + :amount WHERE a.id = :accountId")
+    void updateAccount(String accountId, double amount);
+
+    Account findAccountByIdAndClientID(String accountId, String clientId);
+}
